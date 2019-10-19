e07c60b24f69a53ab05ca1d0c4d025b49d4823d3
winster neal <winster@i.shu.edu.cn>
2019-10-20 05:32:18 +0800

    after commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { CounterService } from './counter.service';
+
+
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+    selector: 'app-root',
+
+    templateUrl: './app.component.html',
+
+    styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'counters';
+
+    constructor(private counterService: CounterService) { }
+
+
+
+    reset() {
+
+        this.counterService.reset()
+
+  }
+
 }
